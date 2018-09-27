eace5b58c88d91f32d1763901070b842558959ec
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-09-26 18:16:05 -0700

    basic file structure

---
+++ test/store.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const Store = require('../lib/store');
+const rootDirectory = path.join(__dirname, 'cats');
+
+describe('store', () => {
+    
+})
