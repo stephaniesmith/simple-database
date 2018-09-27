a6c03f481f0466384de22b035cf8ee8cab119e9c
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-09-26 18:41:36 -0700

    test for saves a cat

--- test/store.test.js
+++ test/store.test.js
@@ -1,9 +1,41 @@
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
 const Store = require('../lib/store');
-const rootDirectory = path.join(__dirname, 'cats');
 
 describe('store', () => {
+
+    const rootDirectory = path.join(__dirname, 'cats');
+
+    beforeEach(done => {
+        rimraf(rootDirectory, err => {
+            if(err && err.code !== 'ENOENT') done(err);
+            else done();
+        });
+    });
+
+    beforeEach(done => {
+        mkdirp(rootDirectory, err => {
+            if(err && err.code !== 'ENOENT') done(err);
+            else done();
+        });
+    });
+
+    it('saves a cat', done => {
+        const store = new Store(rootDirectory);
+        const TC = {
+            name: 'TC'
+        };
+
+        store.save(TC, (err, cat) => {
+            if(err) return don(err);
+
+            assert.ok(cat._id);
+            done();
+        })
+    })
+
     
 })
